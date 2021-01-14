678ef256773abee3f71057759c5e1e00b8b609ff
Calvin Cheung <cheungcalvin2@gmail.com>
2021-01-14 14:08:13 -0500

    Begin moving youtube player to a separate fragment Replace existing youtube player with image and play button

--- app/src/main/java/com/example/recipeapp/recipeDetails/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/recipeapp/recipeDetails/RecipeDetailsFragment.kt
@@ -23,50 +23,50 @@ class RecipeDetailsFragment : Fragment() {
         binding.recipeDetailsViewModel =
             ViewModelProvider(this, viewModelFactory).get(RecipeDetailsViewModel::class.java)
 
-        val youtubePlayer = binding.recipeVideoYoutube
-
-        youtubePlayer.getPlayerUiController().setFullScreenButtonClickListener {
-            Log.d("RecipeDetailsFragment", "Fullscreen clicked")
-            if(youtubePlayer.isFullScreen()) {
-                youtubePlayer.exitFullScreen()
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                    activity?.let { activity ->
-                        activity.window?.let { window ->
-                            window.setDecorFitsSystemWindows(false)
-                            val controller = window.insetsController
-
-                            controller?.show(WindowInsets.Type.navigationBars())
-                        }
-                    }
-
-                } else {
-                    activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-                }
-            } else {
-                youtubePlayer.enterFullScreen()
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                    Log.d("RecipeDetailsFragment", "FULLLLLLSCREEEEEN")
-                    activity?.let { activity ->
-                        activity.window?.let { window ->
-                            window.setDecorFitsSystemWindows(false)
-                            val controller = window.insetsController
-
-                            controller?.hide(WindowInsets.Type.navigationBars())
-                            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                        }
-                    }
-
-                } else {
-                    activity?.window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
-                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            or View.SYSTEM_UI_FLAG_IMMERSIVE
-                            or View.SYSTEM_UI_FLAG_LOW_PROFILE
-                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-                }
-            }
-        }
+//        val youtubePlayer = binding.recipeVideoYoutube
+//
+//        youtubePlayer.getPlayerUiController().setFullScreenButtonClickListener {
+//            Log.d("RecipeDetailsFragment", "Fullscreen clicked")
+//            if(youtubePlayer.isFullScreen()) {
+//                youtubePlayer.exitFullScreen()
+//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+//                    activity?.let { activity ->
+//                        activity.window?.let { window ->
+//                            window.setDecorFitsSystemWindows(false)
+//                            val controller = window.insetsController
+//
+//                            controller?.show(WindowInsets.Type.navigationBars())
+//                        }
+//                    }
+//
+//                } else {
+//                    activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+//                }
+//            } else {
+//                youtubePlayer.enterFullScreen()
+//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+//                    Log.d("RecipeDetailsFragment", "FULLLLLLSCREEEEEN")
+//                    activity?.let { activity ->
+//                        activity.window?.let { window ->
+//                            window.setDecorFitsSystemWindows(false)
+//                            val controller = window.insetsController
+//
+//                            controller?.hide(WindowInsets.Type.navigationBars())
+//                            controller?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//                        }
+//                    }
+//
+//                } else {
+//                    activity?.window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            or View.SYSTEM_UI_FLAG_IMMERSIVE
+//                            or View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+//                }
+//            }
+//        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/recipeapp/utils/YouTubeHelper.kt
@@ -0,0 +1,23 @@
+package com.example.recipeapp.utils
+
+import android.util.Log
+
+class YouTubeHelper {
+    companion object {
+        private fun getId(url: String?): String? {
+            if(!url.isNullOrBlank()) {
+                return url.split("v=")[1]
+            }
+            return null
+        }
+
+        fun getThumbnail(url: String?): String? {
+            if(!url.isNullOrBlank()) {
+                val videoId = getId(url)
+                return "https://img.youtube.com/vi/$videoId/0.jpg"
+            }
+            return null
+        }
+    }
+
+}
